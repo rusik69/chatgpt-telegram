e3779d7850fe79caa95682480bff6da268240d27
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2023-03-22 12:42:58 +0100

    commit

--- pkg/bot/run.go
+++ pkg/bot/run.go
@@ -6,10 +6,12 @@ import (
 	"github.com/rusik69/chatgpt-tg/pkg/env"
 	"github.com/rusik69/chatgpt-tg/pkg/openaiclient"
 	"github.com/rusik69/chatgpt-tg/pkg/telegramclient"
+	"github.com/sashabaranov/go-openai"
 )
 
 // Run runs the bot.
 func Run() {
+	d := map[string][]openai.ChatCompletionMessage{}
 	updates := telegramclient.GetUpdates()
 	for update := range updates {
 		if update.Message != nil {
@@ -18,7 +20,7 @@ func Run() {
 				telegramclient.Send(update, "You are not allowed to use this bot.")
 				continue
 			}
-			message, err := openaiclient.Generate(update.Message.Text)
+			message, err := openaiclient.Generate(update.Message.Text, update.Message.From.UserName, &d)
 			if err != nil {
 				log.Println(err)
 				telegramclient.Send(update, err.Error())
--- pkg/openaiclient/generate.go
+++ pkg/openaiclient/generate.go
@@ -7,22 +7,21 @@ import (
 )
 
 // Generate generates a response.
-func Generate(prompt string) (string, error) {
-	resp, err := Client.CreateChatCompletion(
-		context.Background(),
-		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: prompt,
-				},
-			},
-		},
-	)
-
+func Generate(prompt, user string, d *map[string][]openai.ChatCompletionMessage) (string, error) {
+	(*d)[user] = append((*d)[user], openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
+		Content: prompt,
+	})
+	resp, err := Client.CreateChatCompletion(context.Background(), openai.ChatCompletionRequest{
+		Model:    openai.GPT3Dot5Turbo,
+		Messages: (*d)[user],
+	})
+	content := resp.Choices[0].Message.Content
 	if err != nil {
 		return "", err
 	}
-	return resp.Choices[0].Message.Content, nil
+	(*d)[user] = append((*d)[user], openai.ChatCompletionMessage{
+		Role: openai.ChatMessageRoleAssistant, Content: content,
+	})
+	return content, nil
 }
